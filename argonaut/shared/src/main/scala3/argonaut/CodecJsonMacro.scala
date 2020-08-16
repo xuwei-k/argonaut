92d1dc06772e0b7f23bb4d6633343a151ad2f37c
xuwei-k <6b656e6a69@gmail.com>
2020-08-17 08:36:00 +0900

    wip

--- argonaut/shared/src/main/scala3/argonaut/CodecJsonMacro.scala
+++ argonaut/shared/src/main/scala3/argonaut/CodecJsonMacro.scala
@@ -1,7 +1,5 @@
 package argonaut
 
-import argonaut.internal.CodecDerivation
-
 trait CodecJsonMacro {
   inline def derive[A]: CodecJson[A] =
     internal.Macros.summonCodec[A]
--- argonaut/shared/src/main/scala3/argonaut/internal/Macros.scala
+++ argonaut/shared/src/main/scala3/argonaut/internal/Macros.scala
@@ -14,23 +14,24 @@ object Macros {
 
   inline final def summonEncoder[A]: EncodeJson[A] = summonFrom {
     case encodeA: EncodeJson[A] => encodeA
-    case _: Mirror.ProductOf[A] => EncoderDerivation.derived[A]
+    case _: Mirror.ProductOf[A] => Macros.derivedEncoder[A]
   }
 
   inline final def summonDecoder[A]: DecodeJson[A] = summonFrom {
     case decodeA: DecodeJson[A] => decodeA
-    case _: Mirror.ProductOf[A] => DecoderDerivation.derived[A]
+    case _: Mirror.ProductOf[A] => Macros.derivedDecoder[A]
   }
 
   inline final def summonCodec[A]: CodecJson[A] = summonFrom {
     case codecA: CodecJson[A] => codecA
-    case _: Mirror.ProductOf[A] => CodecDerivation.derived[A]
+    case _: Mirror.ProductOf[A] => Macros.derivedCodec[A]
   }
 
-  inline final def summonLabelsRec[T <: Tuple]: List[String] = inline erasedValue[T] match {
-    case _: EmptyTuple => Nil
-    case _: (t *: ts) => constValue[t].asInstanceOf[String] :: summonLabelsRec[ts]
-  }
+  inline final def summonLabelsRec[T <: Tuple]: List[String] =
+    inline erasedValue[T] match {
+      case _: EmptyTuple => Nil
+      case _: (t *: ts) => constValue[t].asInstanceOf[String] :: summonLabelsRec[ts]
+    }
 
   inline final def summonDecodersRec[T <: Tuple]: List[DecodeJson[_]] =
     inline erasedValue[T] match {
@@ -43,10 +44,8 @@ object Macros {
       case _: EmptyTuple => Nil
       case _: (t *: ts) => summonEncoder[t] :: summonEncodersRec[ts]
     }
-}
 
-trait EncoderDerivation {
-  inline final def derived[A](using inline A: Mirror.ProductOf[A]): EncodeJson[A] =
+  inline final def derivedEncoder[A](using inline A: Mirror.ProductOf[A]): EncodeJson[A] =
     new EncodeJson[A] with DerivedEncoder[A] {
       override val elemEncoders: Array[EncodeJson[_]] =
         Macros.summonEncoders[A.MirroredElemTypes]
@@ -57,21 +56,14 @@ trait EncoderDerivation {
         )
       }
     }
-}
 
-object EncoderDerivation extends EncoderDerivation
-
-object CodecDerivation {
-  inline final def derived[A](using inline A: Mirror.ProductOf[A]): CodecJson[A] =
+  inline final def derivedCodec[A](using inline A: Mirror.ProductOf[A]): CodecJson[A] =
     CodecJson.derived[A](
-      E = EncoderDerivation.derived[A],
-      D = DecoderDerivation.derived[A],
+      E = derivedEncoder[A],
+      D = derivedDecoder[A],
     )
-}
 
-trait DecoderDerivation {
-
-  inline final def derived[A](using inline A: Mirror.ProductOf[A]): DecodeJson[A] =
+  inline final def derivedDecoder[A](using inline A: Mirror.ProductOf[A]): DecodeJson[A] =
     new DerivedDecoder[A] {
       private[this] val elemLabels0 = Macros.summonLabels[A.MirroredElemLabels]
       override def elemLabels(i: Int): String = elemLabels0(i)
@@ -109,8 +101,6 @@ trait DecoderDerivation {
       }
     }
 }
-
-object DecoderDerivation extends DecoderDerivation
 
 trait DerivedEncoder[A] extends EncodeJson[A] {
   protected[this] def elemEncoders: Array[EncodeJson[_]]
